ffc72ee5d3ead1bc2dd3835d5ebc7154edcc4eee
Coru <74803005+Corubel@users.noreply.github.com>
2023-01-11 20:41:28 -0300

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "tarea1",
+    description: "algo que hacer..",
+  },
+  {
+    id: 1,
+    title: "tarea2",
+    description: "para despues",
+  },
+  {
+    id: 2,
+    title: "tarea3",
+    description: "para mañana",
+  },
+];
